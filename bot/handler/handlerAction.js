2b010936013f490bc4de2c29fd7c0419bbf8785d
Shohagbot <skshohagislambd@gmail.com>
2024-11-01 09:59:10 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -72,8 +72,8 @@ mentions:[{id:event.senderID, tag:nname}],
 				break;
 			case "message_reaction":
 				onReaction();
-        if(event.reaction == "😠"){
-  if(event.userID == "100058695235956"){
+        if(event.reaction == "❌"){
+  if(event.userID == "shohag.islam.fb1"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -82,8 +82,8 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send(":)")
   }
   }
-        if(event.reaction == "😾"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100058695235956"){
+        if(event.reaction == "🪓"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "shohag.islam.fb1"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
